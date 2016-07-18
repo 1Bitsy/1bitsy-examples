23b000054bc7dda83a407f490abb1c174a095db8
kbob <kbob@jogger-egg.com>
2016-07-18 10:43:12 -0700

    Reindented.

--- examples/1bitsy/i2s-warble/i2s-warble.c
+++ examples/1bitsy/i2s-warble/i2s-warble.c
@@ -48,51 +48,52 @@ static void setup_clocks(void)
 #if 0
 static void tim_setup(void)
 {
-	/* Reset TIM1 peripheral. */
-	timer_reset(TIM1);
-	/* Timer global mode:
-	* - No divider
-	* - Alignment edge
-	* - Direction up
-	*/
-	timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
-	timer_set_prescaler(TIM1, 0);
-	timer_set_repetition_counter(TIM1, 0);
-	/* Enable preload. */
-	timer_disable_preload(TIM1);
-	/* Continous mode. */
-	timer_continuous_mode(TIM1);
-	/* Period (36kHz). */
-	timer_set_period(TIM1, 0xFFFF);
-	/* Disable outputs. */
-	timer_disable_oc_output(TIM1, TIM_OC1);
-	timer_disable_oc_output(TIM1, TIM_OC2);
-	timer_disable_oc_output(TIM1, TIM_OC3);
-	timer_disable_oc_output(TIM1, TIM_OC4);
+    /* Reset TIM1 peripheral. */
+    timer_reset(TIM1);
+    /* Timer global mode:
+     * - No divider
+     * - Alignment edge
+     * - Direction up
+     */
+    timer_set_mode(TIM1, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+    timer_set_prescaler(TIM1, 0);
+    timer_set_repetition_counter(TIM1, 0);
+    /* Enable preload. */
+    timer_disable_preload(TIM1);
+    /* Continous mode. */
+    timer_continuous_mode(TIM1);
+    /* Period (36kHz). */
+    timer_set_period(TIM1, 0xFFFF);
+    /* Disable outputs. */
+    timer_disable_oc_output(TIM1, TIM_OC1);
+    timer_disable_oc_output(TIM1, TIM_OC2);
+    timer_disable_oc_output(TIM1, TIM_OC3);
+    timer_disable_oc_output(TIM1, TIM_OC4);
 
-	timer_set_deadtime(TIM1, 0);
-	timer_set_enabled_off_state_in_idle_mode(TIM1);
-	timer_set_enabled_off_state_in_run_mode(TIM1);
-	timer_disable_break(TIM1);
-	timer_set_break_polarity_high(TIM1);
-	timer_disable_break_automatic_output(TIM1);
-	timer_set_break_lock(TIM1, TIM_BDTR_LOCK_OFF);
+    timer_set_deadtime(TIM1, 0);
+    timer_set_enabled_off_state_in_idle_mode(TIM1);
+    timer_set_enabled_off_state_in_run_mode(TIM1);
+    timer_disable_break(TIM1);
+    timer_set_break_polarity_high(TIM1);
+    timer_disable_break_automatic_output(TIM1);
+    timer_set_break_lock(TIM1, TIM_BDTR_LOCK_OFF);
 
-	/* -- OC1 configuration -- */
-	/* Configure global mode of line 1. */
-	timer_disable_oc_clear(TIM1, TIM_OC1);
-	timer_enable_oc_preload(TIM1, TIM_OC1);
-	timer_set_oc_slow_mode(TIM1, TIM_OC1);
-	timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
-	timer_set_oc_polarity_high(TIM1, TIM_OC1);
-	timer_set_oc_idle_state_set(TIM1, TIM_OC1);
-	/* Set the capture compare value for OC1 to max value -1 for max duty cycle/brightness. */
-	timer_set_oc_value(TIM1, TIM_OC1, 0xFFFF/2);
-	timer_enable_oc_output(TIM1, TIM_OC1);
-	timer_enable_preload(TIM1);
-	timer_enable_break_main_output(TIM1);
-	/* Counter enable. */
-	timer_enable_counter(TIM1);
+    /* -- OC1 configuration -- */
+    /* Configure global mode of line 1. */
+    timer_disable_oc_clear(TIM1, TIM_OC1);
+    timer_enable_oc_preload(TIM1, TIM_OC1);
+    timer_set_oc_slow_mode(TIM1, TIM_OC1);
+    timer_set_oc_mode(TIM1, TIM_OC1, TIM_OCM_PWM1);
+    timer_set_oc_polarity_high(TIM1, TIM_OC1);
+    timer_set_oc_idle_state_set(TIM1, TIM_OC1);
+    /* Set the capture compare value for OC1 to max value -1 for max
+       duty cycle/brightness. */
+    timer_set_oc_value(TIM1, TIM_OC1, 0xFFFF/2);
+    timer_enable_oc_output(TIM1, TIM_OC1);
+    timer_enable_preload(TIM1);
+    timer_enable_break_main_output(TIM1);
+    /* Counter enable. */
+    timer_enable_counter(TIM1);
 
 }
 #endif
@@ -166,35 +167,35 @@ static void setup_button(void)
 
 int main(void)
 {
-	setup_clocks();
-        setup_systick(168000000);
-	// tim_setup();
-        // i2s_setup();
-        setup_console();
-        setup_console_stdio();
-        setup_LEDs();
-        setup_button();
+    setup_clocks();
+    setup_systick(168000000);
+    // tim_setup();
+    // i2s_setup();
+    setup_console();
+    setup_console_stdio();
+    setup_LEDs();
+    setup_button();
 
-        printf("Hello, World!\n");
+    printf("Hello, World!\n");
 
-        uint32_t next = 500;
-        while (true) {
-            uint32_t now = system_millis;
-            if (now == next) {
-                gpio_toggle(GPIOB, GPIO0);
-                next += 500;
+    uint32_t next = 500;
+    while (true) {
+        uint32_t now = system_millis;
+        if (now == next) {
+            gpio_toggle(GPIOB, GPIO0);
+            next += 500;
+        }
+        if (debounce_update(&button)) {
+            if (debounce_is_falling_edge(&button)) {
+                gpio_clear(GPIOA, GPIO8);
+                printf("Click-"); fflush(stdout);
             }
-            if (debounce_update(&button)) {
-                if (debounce_is_falling_edge(&button)) {
-                    gpio_clear(GPIOA, GPIO8);
-                    printf("Click-"); fflush(stdout);
-                }
-                if (debounce_is_rising_edge(&button)) {
-                    gpio_set(GPIOA, GPIO8);
-                    printf("clack\n");
-                }
+            if (debounce_is_rising_edge(&button)) {
+                gpio_set(GPIOA, GPIO8);
+                printf("clack\n");
             }
         }
+    }
 
-	return 0;
+    return 0;
 }
