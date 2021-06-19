f772c5da8198a105611b0182331e3b70c57c5c47
Piotr Esden-Tempski <piotr@esden.net>
2021-06-18 22:12:15 -0700

    button: Corrected rcc init call.

--- examples/1bitsy/button/button.c
+++ examples/1bitsy/button/button.c
@@ -29,7 +29,7 @@ uint16_t exti_line_state;
 /* Set STM32 to 168 MHz. */
 static void clock_setup(void)
 {
-	rcc_clock_setup_hse_3v3(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
+	rcc_clock_setup_pll(&rcc_hse_25mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 }
 
 static void gpio_setup(void)
